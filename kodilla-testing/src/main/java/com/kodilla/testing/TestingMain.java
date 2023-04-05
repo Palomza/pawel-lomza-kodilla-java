9a90fec26cdab7f2da120a117a8f67a04030da8e
Pawel Lomza <pawel.lomza4@gmail.com>
2023-04-05 17:26:25 +0200

    6.2 Added Calculator and SimpleUser packages and tested them in TestingMain

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowanai oprogramowani");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        if(5 == calculator.add(2, 3)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if(3 == calculator.subtract(5, 2)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
