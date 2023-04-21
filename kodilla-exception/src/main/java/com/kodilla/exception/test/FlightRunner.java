3597b9ee687e00a65b0e262d72890407eded72f6
Pawel Lomza <pawel.lomza4@gmail.com>
2023-04-21 18:23:34 +0200

    8.4 Added three classes all around findFlight method

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -0,0 +1,64 @@
+package com.kodilla.exception.test;
+
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightRunner {
+
+    private Map<String, Boolean> flightMap;
+
+    public FlightRunner() {
+        flightMap = new HashMap<>();
+        flightMap.put("BXP", true);
+        flightMap.put("WAW", true);
+        flightMap.put("SZY", false);
+        flightMap.put("GDN", false);
+    }
+
+    public void findFlight(Flight flight) throws RouteNotFoundException{
+        String departureAirport = flight.getDepartureAirport();
+        String arrivalAirport = flight.getArrivalAirport();
+
+        if(!flightMap.containsKey(departureAirport) || !flightMap.containsKey(arrivalAirport)){
+            throw new RouteNotFoundException("Can't make that flight, one or both airports not found.");
+        }
+
+        boolean isArrivalAvailable = flightMap.get(arrivalAirport);
+        boolean isDepartureAvailable = flightMap.get(departureAirport);
+
+        if(!isArrivalAvailable || !isDepartureAvailable){
+            throw new RouteNotFoundException("Can't make that flight, one or both airports are not operational.");
+        }
+
+        System.out.println("Flight: " + departureAirport + " - " + arrivalAirport + " is available.");
+    }
+
+    public static void main(String[] args) {
+        FlightRunner flightRunner = new FlightRunner();
+        Flight flight1 = new Flight("BXP", "FLY");
+        Flight flight2 = new Flight("BXP", "WAW");
+        Flight flight3 = new Flight("WAW", "SZY");
+
+        //Airport does not exists in flightMap
+        try {
+            flightRunner.findFlight(flight1);
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //Both departure and arrival airports have true values in flightmap
+        try {
+            flightRunner.findFlight(flight2);
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //One airport have true value in flightmap and other have false value in flightmap
+        try {
+            flightRunner.findFlight(flight3);
+        } catch (RouteNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
