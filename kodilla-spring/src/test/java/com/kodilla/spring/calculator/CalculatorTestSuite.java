6370b1fbc1128e05c6a2663c9939381c0f89b909
Pawel Lomza <pawel.lomza4@gmail.com>
2023-06-01 22:25:27 +0200

    14.2 CalculatorTestSuite split one into several

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -12,18 +12,37 @@ class CalculatorTestSuite {
     private Calculator calculator;
 
     @Test
-    void testCalculations() {
+    void testCalculatorAdd() {
         //Given
         //When
         double addResult = calculator.add(2.0, 4.0);
-        double subResult = calculator.sub(2.0, 4.0);
-        double mulResult = calculator.mul(2.0, 4.0);
-        double divResult = calculator.div(2.0, 4.0);
         //Then
         Assertions.assertEquals(6.0, addResult);
-        Assertions.assertEquals(-2.0, subResult);
-        Assertions.assertEquals(8.0, mulResult);
-        Assertions.assertEquals(0.5, divResult);
+    }
 
+    @Test
+    void testCalculatorSub() {
+        //Given
+        //When
+        double subResult = calculator.sub(2.0, 4.0);
+        //Then
+    }
+
+    @Test
+    void testCalculatorMul() {
+        //Given
+        //When
+        double mulResult = calculator.mul(2.0, 4.0);
+        //Then
+        Assertions.assertEquals(8.0, mulResult);
+    }
+
+    @Test
+    void testCalculatorDiv() {
+        //Given
+        //When
+        double divResult = calculator.div(2.0, 4.0);
+        //Then
+        Assertions.assertEquals(0.5, divResult);
     }
 }
