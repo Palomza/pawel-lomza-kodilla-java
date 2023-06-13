1da7364d55c989f80a42e8cbd9b4bd8b34b2d3ab
Pawel Lomza <pawel.lomza4@gmail.com>
2023-06-13 22:16:19 +0200

    16.6 Active users

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.jdbc;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -66,5 +67,33 @@ class DbManagerTestSuite {
         rs.close();
         statement.close();
         Assertions.assertEquals(8, counter);
+    }
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = """
+                SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POST_NUMBER
+                FROM USERS U JOIN POSTS P on U.ID = P.USER_ID
+                GROUP BY P.USER_ID
+                HAVING POST_NUMBER >= 2
+                """;
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("FIRSTNAME") + ", " +
+                    resultSet.getString("LASTNAME") + ", " +
+                    resultSet.getInt("POST_NUMBER"));
+            counter++;
+        }
+        resultSet.close();
+        statement.close();
+        Assertions.assertEquals(1, counter);
     }
 }
